9065ec58fe3d577168391f006da237f81930cf31
zhakanov <67269797+zhakanov@users.noreply.github.com>
2023-01-18 16:20:02 +0600

    Update archive_app.sh

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -6,5 +6,6 @@ xcodebuild -workspace Calculator.xcworkspace \
             -scheme Calculator\ iOS \
             -sdk iphoneos \
             -configuration AppStoreDistribution \
+            -destination $PWD/build/Calculator.xcarchive \
             -archivePath $PWD/build/Calculator.xcarchive \
             clean archive | xcpretty
